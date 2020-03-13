71fb2de49f59cbbd079be060264597113e2dcb15
turovtsev.yaroslav <turovtsev.yaroslav@euroset.ru>
2020-03-14 01:24:30 +0300

    28

--- 28.implement-str-str.js
+++ 28.implement-str-str.js
@@ -6,12 +6,18 @@
 
 // @lc code=start
 /**
- * @param {string} haystack
- * @param {string} needle
+ * @param {string} h
+ * @param {string} n
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-    return haystack.indexOf(needle);
+var strStr = function(h, n) {
+    // return h.indexOf(n);
+    if (!n) return 0;
+    for (let i = 0; i < h.length; i++) {
+        if (n === h.slice(i, i + n.length)) {
+            return i;
+        }
+    }
+    return -1;
 };
 // @lc code=end
-
