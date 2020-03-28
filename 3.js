cae390809d2f01ea5505182a679ee6b3cb7777bd
Yaroslav <yturovtsev@gmail.com>
2020-03-28 22:15:58 +0300

    New solve anagramm

--- 3.js
+++ 3.js
@@ -1,29 +1,39 @@
-const str = 'среда фывап среад ыафпв дерас торс сорт трос';
+const str = 'среда торс фывап среад ыафпв дерас сорт трос';
 
 const foo = str => {
   const arr = str.split(' ');
-  const l = arr.length;
-  const result = [];
+  // const l = arr.length;
+  // const result = [];
   const sort = str => str.split('').sort().join('');
 
-  for (let i = 0; i < l; i++) {
-    if (arr[i] === '') continue;
-    const word = arr[i];
-    const sortedWord = sort(arr[i]);
-    const localRes = [word];
+  // for (let i = 0; i < l; i++) {
+  //   if (arr[i] === '') continue;
+  //   const word = arr[i];
+  //   const sortedWord = sort(arr[i]);
+  //   const localRes = [word];
 
-    for (let j = i + 1; j < l; j++) {
-      const innerWord = arr[j];
-      const sortedInnerWord = sort(arr[j]);
+  //   for (let j = i + 1; j < l; j++) {
+  //     const innerWord = arr[j];
+  //     const sortedInnerWord = sort(arr[j]);
 
-      if (sortedWord === sortedInnerWord) {
-        localRes.push(innerWord);
-        arr[j] = '';
-      }
-    }
-    result.push(localRes);
-  }
-  return result;
+  //     if (sortedWord === sortedInnerWord) {
+  //       localRes.push(innerWord);
+  //       arr[j] = '';
+  //     }
+  //   }
+  //   result.push(localRes);
+  // }
+
+  const reduced = arr.reduce((acc, str) => {
+    const sortedWord = sort(str);
+
+    acc[sortedWord] = [ ...(acc[sortedWord] || []), str];  
+
+    return acc;
+  }, {});
+
+  return Object.values(reduced);
+  // return result;
 };
 
 console.log(foo(str));
